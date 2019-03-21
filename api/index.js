fb6cca825af0ed77eec47f04f013910784d41aff
neshka <neshka_mind@yahoo.com>
2019-03-21 18:23:39 +0000

    add authentication

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -27,6 +28,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 //add port
 const port = process.env.PORT || 4000;
