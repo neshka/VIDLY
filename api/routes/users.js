28dd3cfb7d7632fb6554f2307546382ef84fbc1b
neshka <neshka_mind@yahoo.com>
2019-03-21 16:59:56 +0000

    add bcrypt

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,9 @@
+const _ = require("lodash");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
 
 //read all genres
 router.post("/", async (req, res) => {
@@ -12,14 +14,22 @@ router.post("/", async (req, res) => {
   if (user)
     return res.status(400).send("User with this email already registered");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  //   user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password
+  //   });
+
+  //we can use pick for code above:
+
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
