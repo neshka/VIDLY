64fb5f15929982d416a58ed8f2b6be971399b468
neshka <neshka_mind@yahoo.com>
2019-04-07 23:07:42 +0100

    extract config logic

--- api/index.js
+++ api/index.js
@@ -1,29 +1,10 @@
-const winston = require("winston");
-require("winston-mongodb");
 const config = require("config");
 const express = require("express");
 const app = express();
 
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-process.on("uncaughtException", ex => {
-  //throw ex;
-});
-
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", ex => {
-  //throw ex;
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, {
-  db: "mongodb://localhost/vidly",
-  level: "info"
-});
 
 const p = Promise.reject(new Error("Something failed miserably!"));
 p.then(() => console.log("Done"));
