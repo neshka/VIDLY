1552a3429c37ba2a12cd932003fc37c79db40940
neshka <neshka_mind@yahoo.com>
2019-04-07 22:39:58 +0100

    add winston logs

--- api/index.js
+++ api/index.js
@@ -14,8 +14,30 @@ const auth = require("./routes/auth");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+process.on("uncaughtException", ex => {
+  // winston.error(ex.message, ex);
+  //process.exit(1); //anything but 0 means failure
+  //throw ex;
+});
+
+winston.handleExceptions(
+  new winston.transports.File({ filename: "uncaughtExceptions.log" })
+);
+
+process.on("unhandledRejection", ex => {
+  // winston.error(ex.message, ex);
+  //process.exit(1);
+  //throw ex;
+});
+
 winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, { db: "mongodb://localhost/vidly" });
+winston.add(winston.transports.MongoDB, {
+  db: "mongodb://localhost/vidly",
+  level: "info"
+});
+
+const p = Promise.reject(new Error("Something failed miserably!"));
+p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
