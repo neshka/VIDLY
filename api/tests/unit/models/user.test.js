30a58c3b5fc0223d5985a6e0a8a4499026149a7c
neshka <neshka_mind@yahoo.com>
2019-04-08 00:23:08 +0100

    add test folder

---
+++ api/tests/unit/models/user.test.js
@@ -0,0 +1,12 @@
+const { User } = require("../../../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+describe("user.generateAuthToken", () => {
+  it("should return a valid JWT", () => {
+    const user = new User({ _id: 1, isAdmin: true });
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject({ _id: 1, isAdmin: true });
+  });
+});
